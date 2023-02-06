36feab37a7ac34e11529d7bf0ecc6db03806bf0f
jia-wei-teh <jwteh5695@hotmail.com>
2023-02-07 01:44:23 +0800

    remove error

--- src/entity/inventory.entity.ts
+++ src/entity/inventory.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';  from "typeorm";
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('inventory')
 export class Inventory {
