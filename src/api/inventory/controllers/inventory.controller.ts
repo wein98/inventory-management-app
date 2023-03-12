d4df18d5672fa56b0caa849b1a8db8dcb331ddae
jia-wei-teh <jwteh5695@hotmail.com>
2023-03-12 14:00:06 +0800

    upload CSV to get sales items

--- src/api/inventory/controllers/inventory.controller.ts
+++ src/api/inventory/controllers/inventory.controller.ts
@@ -12,13 +12,24 @@ import {
     Query,
     NotFoundException,
     Req,
+    BadRequestException
   } from "@nestjs/common";
-  import { ApiBearerAuth, ApiBody, ApiConsumes, ApiOkResponse, ApiQuery, ApiTags } from "@nestjs/swagger";
+import { ApiBearerAuth, ApiBody, ApiConsumes, ApiOkResponse, ApiQuery, ApiTags } from "@nestjs/swagger";
+import { FileInterceptor } from "@nestjs/platform-express"
 import { InjectRepository } from "@nestjs/typeorm";
 import { ProductVariation } from "src/entity/product-variation.entity";
 import { Product } from "src/entity/product.entity";
 import { Repository } from "typeorm";
   
+var csv = require("csvtojson"); 
+
+export const imageFileFilter = (req:any, file:any, callback:any) => {
+  if (!file.originalname.match(/\.(csv)$/)) {
+    return callback(new Error('Only csv files are allowed!'), false);
+  }
+  callback(null, true);
+};
+
   @ApiTags("Inventory")
   @Controller("/api/inventory")
   export class InventoryController {
@@ -28,8 +39,57 @@ import { Repository } from "typeorm";
         @InjectRepository(ProductVariation)
         private readonly productVariationRepository: Repository<ProductVariation>
     ) {}
+
+
+    // Get the uploaded CSV
+    @Post('sales/import/')
+    @ApiConsumes('multipart/form-data')
+    @ApiBody({
+      schema: {
+        type: "object",
+        properties: {
+          "file": {
+              type: "file",
+              format: "binary",
+          }
+        },
+        required: ["file"]
+      }
+    })
+    @ApiOkResponse({ type: Array<Object> })
+    @UseInterceptors(
+      FileInterceptor('file', {
+        fileFilter: imageFileFilter,
+      }),
+    )
+    async importFile(@Body() body, @UploadedFile() file: Express.Multer.File){
+      try {
+        if (!file) {
+          throw new BadRequestException('invalid file provided, allowed *.csv single file');
+        }
+        // convert csv to json 
+        let data = await csv()
+          .fromString(file.buffer.toString())
+          .then(jsonObj=>jsonObj);
+
+        console.log(data)
+
+        let viewItems = data.map ( sale => {
+          console.log(sale.orderId)
+          console.log(sale.orderAmount)
+        })
+
+        return data
+        
+      } catch(err){
+        throw err;
+      }
+
+    }
+
   
     // GET stocks of distinct product variation by productId
+
 
     // GET total weight by productId
 
